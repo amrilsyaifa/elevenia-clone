57546e5a1e4b7017ef772c9229096db315c4ab35
Amril Syaifa Yasin <38855488+amrilsyaifa@users.noreply.github.com>
2022-06-10 23:19:38 +0700

    Feature: Setup Redux, Redux-persist, Redux-thunk

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import AppRouter from './App.Router';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { persistor, store } from 'src/Store';
 
 const App = () => {
-  return <AppRouter />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <AppRouter />
+      </PersistGate>
+    </Provider>
+  );
 };
 
 export default App;
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { useAppSelector, useAppDispatch } from 'src/Hooks/useReduxHooks';
+import { decrement, increment } from 'src/Store/Reducers/Auth.Reducers';
 import { API_KEY } from 'src/Reusables/Constants';
 
 const Home = () => {
-  return <div>Home, {API_KEY}</div>;
+  // The `state` arg is correctly typed as `RootState` already
+  const count = useAppSelector((state) => state.auth.value);
+  const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      <div>Home, {API_KEY}</div>
+      <div>
+        <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <span>{count}</span>
+        <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
