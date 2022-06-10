b49ff01df9d4171ec4ab300767117b3c9b33be93
Amril Syaifa Yasin <38855488+amrilsyaifa@users.noreply.github.com>
2022-06-10 21:22:06 +0700

    Add Env

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { API_KEY } from 'src/Reusables/Constants';
 
 const Home = () => {
-  return <div>Home</div>;
+  return <div>Home, {API_KEY}</div>;
 };
 
 export default Home;
